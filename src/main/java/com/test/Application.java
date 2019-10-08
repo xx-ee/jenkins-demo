3d51d080653d22b8d252c49885914303c2ab45d6
dong <dxiesjz@163.com>
2019-10-08 11:11:05 +0800

    modify pom.xml

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -1,13 +1,13 @@
 package com.test;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@Component("jenkins-demo")
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
